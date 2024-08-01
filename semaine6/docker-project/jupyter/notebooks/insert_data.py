e82a9b95619245ac2474fadcebca07016a035a99
laien <laienwu0112@gmail.com>
2024-08-01 03:49:16 +0200

    first working version of jupyter

---
+++ semaine6/docker-project/jupyter/notebooks/insert_data.py
@@ -0,0 +1,31 @@
+# from dotenv import load_dotenv
+# import os
+from pymongo import MongoClient, InsertOne
+from bson.json_util import loads
+
+
+if __name__ == '__main__':
+    # load_dotenv()
+    # usr = os.getenv("mongo_usr")
+    # pwd = os.getenv("mongo_pw")
+    # with MongoClient(f'mongodb://{usr}:{pwd}@mongodb:27017/') as client:
+    with MongoClient(f'mongodb://usr:pw@mongodb:27017/') as client:
+        db = client["airbnb"]
+        collection = db["listingsAndReviews"]
+
+        if "listingsAndReviews" in client.airbnb.list_collection_names():
+            client.airbnb.listingsAndReviews.drop()
+
+        collection = client.airbnb.listingsAndReviews
+        result = []
+        with open('listingsAndReviews.json', 'r', encoding='utf8') as f:
+            for jsonObj in f:
+                try:
+                    my_json = loads(jsonObj)
+                    result.append(InsertOne(my_json))
+                except Exception as e:
+                    print(f"Error processing JSON: {e.args}")
+
+        collection.bulk_write(result)
+        print(collection.count_documents({}))
+
